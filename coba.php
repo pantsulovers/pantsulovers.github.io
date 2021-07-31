ad8f361b4d186b856d23f793830d45aae69056da
pantsulovers <34789579+pantsulovers@users.noreply.github.com>
2021-08-01 02:42:14 +0700

    Add files via upload

---
+++ coba.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>abcd</title>
+</head>
+<body>
+		<?php  
+		$x = 1;
+		$z= 1;
+		while($z < 20000) {
+			if ($x>1000000) {
+				echo "$z .The number is: $x <br>";
+				if ($x>10000000) {
+				echo "$z .wuhan kontol: $x <br>";
+			}
+			}
+			$x=$x+1+$z+5/100;
+			$z++;
+		}
+			?>
+</body>
+</html>
